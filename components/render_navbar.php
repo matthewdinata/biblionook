0768d0c15dbb796a2436fd7f2be091e78f9cdb9e
gabriellenicole <gabrielle.nicole2003@gmail.com>
2024-10-19 00:27:28 +0800

    feat: update navbar

--- components/render_navbar.php
+++ components/render_navbar.php
@@ -2,17 +2,21 @@
 function renderNavbar($currentPage)
 {
     $menu_items = [
-        'Home' => 'index.php',
-        'Search' => 'search.php',
-        'Your Books' => 'books.php',
-        'Pricing' => 'pricing.php',
+        'Home' => ['url' => 'index.php', 'img' => './assets/icons/home.svg'],
+        'Search' => ['url' => 'search.php', 'img' => './assets/icons/search.svg'],
+        'Your Books' => ['url' => 'books.php', 'img' => './assets/icons/books.svg'],
+        'Pricing' => ['url' => 'pricing.php', 'img' => './assets/icons/pricing.svg'],
     ];
 
     echo '<nav><ul>';
 
-    foreach ($menu_items as $title => $url) {
+    foreach ($menu_items as $title => $item) {
+        $url = $item['url'];
+        $img_src = $item['img'];
         $class = ($currentPage === $url) ? ' class="active"' : '';
-        echo "<a href=\"$url\"$class><li>$title</li></a>";
+        echo "<a href=\"$url\"$class>
+            <li><img src=\"$img_src\" alt=\"$title icon\" /> $title</li>
+        </a>";
     }
     echo '</ul></nav>';
 }
--- index.php
+++ index.php
@@ -6,16 +6,36 @@
     <link rel="stylesheet" href="css/globals.css" />
     <link rel="stylesheet" href="css/components.css" />
     <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="css/index.css" />
 </head>
 
 <body>
-    <h1>Home</h1>
-
     <?php
     require_once 'components/render_navbar.php';
     $current_page = basename($_SERVER['PHP_SELF']);
     renderNavbar($current_page);
-    ?>
+    ?>x
+    <div class="main-content">
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+        <h1>Home</h1><br />
+    </div>
 </body>
 
 </html>
